8b0897a74f61ed44970b25cb2ba24e3e0fa546e6
Admin <admin@Michaels-MacBook-Pro.local>
2020-06-09 09:26:47 +0200

    Add comments to JS code

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ const height = document.querySelector('.td-height')
 const characterArr = [];
 
 function addToArr (res) {
+  // Add current set of results to the character array
   const resultsArr = res.data.results;
   resultsArr.forEach((obj) => {
     characterArr.push(obj);
   })
+  // Get next set of results
   const nextURL = res.data.next;
   return axios.get(nextURL);
 }
 
+// GET THE CHARACTER DATA FROM THE STAR WARS API
 axios.get('https://swapi.dev/api/people/?page=1')
 .then((res) => addToArr(res))
 .then((res) => addToArr(res))
@@ -27,7 +30,9 @@ axios.get('https://swapi.dev/api/people/?page=1')
 .then((res) => addToArr(res))
 .then((res) => {
   console.log('Ready');
+  // Event listener for submit button
   submit.addEventListener('click', (e) => {
+    // Populate the selection box on UI with the character the user has sumbitted
     characterArr.forEach((char) => {
       if (input.value === char.name) {
         name.innerText = char.name;
@@ -36,10 +41,31 @@ axios.get('https://swapi.dev/api/people/?page=1')
         height.innerText = char.height;
       }
     })
+    // Reset the input to blank
+    input.value = '';
     e.preventDefault();
   })
 })
 
+
+
+
+//  ADD CHARACTER TO TEAM ON CLICK
+document.querySelector('.add-button').addEventListener('click', () => {
+  // Instantiate new character from selection box
+  const char = new Character(name.innerText, eyecol.innerText, weight.innerText, height.innerText);
+  // Add the character to the team-member box
+  const ui = new UI();
+  ui.addToUi(char);
+  // Clear the fields in the selection box
+  name.innerText = '';
+  eyecol.innerText = '';
+  height.innerText = '';
+  weight.innerText = '';
+})
+
+
+// CREATE THE CHARACTER CLASS
 class Character {
   constructor(name, eyecol, weight, height) {
     this.name = name;
@@ -49,14 +75,9 @@ class Character {
   }
 }
 
-
-document.querySelector('.add-button').addEventListener('click', () => {
-  const char = new Character(name.innerText, eyecol.innerText, weight.innerText, height.innerText);
-  const ui = new UI();
-  ui.addToUi(char);
-})
-
+// UI METHODS
 class UI {
+  // ADD SELECTION TO TEAM (UI)
   addToUi(char) {
     const row = document.createElement('tr');
     row.innerHTML = `
@@ -64,6 +85,7 @@ class UI {
       <td>${char.eyecol}</td>
       <td>${char.weight}</td>
       <td>${char.height}</td>
+      <td>X</td>
       `
     const table = document.querySelector('.table2');  
     table.appendChild(row);
