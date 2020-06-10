a3cc044d93a0eafdf64f017495b791c07b3334ca
Admin <admin@Michaels-MacBook-Pro.local>
2020-06-10 06:27:55 +0200

    add type ahead functionality to input field

--- app.js
+++ app.js
@@ -32,8 +32,9 @@ axios.get('https://swapi.dev/api/people/?page=1')
   console.log('Ready');
   // Event listener for submit button
   submit.addEventListener('click', (e) => {
+    const ui = new UI();
     if (input.value === '') {
-      alert('you need to enter a valid star wars character')
+      ui.createAlert('Please enter a valid Star Wars character', 'failure');
     } else {
     // Populate the selection box on UI with the character the user has sumbitted
     characterArr.forEach((char) => {
@@ -44,16 +45,53 @@ axios.get('https://swapi.dev/api/people/?page=1')
         height.innerText = char.height;
   } 
 })
-
-    }
-    
-    // Reset the input to blank
-    input.value = '';
-    e.preventDefault();
+    }   
+     // Reset the input to blank
+     input.value = '';
+     e.preventDefault();
+    })
   })
-})
+  
+  // CREATE THE INPUT FIELD TYPE AHEAD FEATURE
+  // Create the filter according to the letters entered into the input
+  function findMatches (wordToMatch, array) {
+    return array.filter((obj) => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return obj.name.match(regex);
+    })
+   }
+  
+   // Event listeners on the input field
+  input.addEventListener('keyup', displayMatches);
+  input.addEventListener('change', displayMatches);
 
+  // Append the filtered array of results to the UI
+  function displayMatches () {
+    const matches = findMatches(input.value, characterArr);
+    // Creating the HTML to append to the UL
+    const html = matches.map((obj) => {
+      return `
+        <li>
+          <span class='name'>${obj.name}</span>
+        </li>
+      `
+    }).join('')
+    const list = document.querySelector('.filter-list');
+    list.innerHTML = html;
+    // When search field is empty, filtered array mustn\'t display
+    if (input.value === '') {
+      list.innerHTML = '';
+    }
+    // When an item is selected from the filtered array, it must populate to the input field
+    const allList = document.querySelectorAll('.filter-list');
+    allList.forEach((obj, i) => {
+      allList[i].addEventListener('click', (e) => {
+          input.value = e.target.innerText;
+          list.innerHTML = '';
+      })
+    })
 
+  }
 
 
 //  ADD CHARACTER TO TEAM ON CLICK
@@ -62,13 +100,15 @@ document.querySelector('.add-button').addEventListener('click', () => {
   // Check to see if there are already three team members
   const table = document.querySelectorAll('.table2 tr');
   console.log()
-  if (table.length === 2) {
-    ui.createAlert('Didnt fucking work mate', 'failure')
+  if (table.length === 4) {
+    ui.createAlert('You can only have three team members', 'failure')
   } else {
   // Instantiate new character from selection box
   const char = new Character(name.innerText, eyecol.innerText, weight.innerText, height.innerText);
   // Add the character to the team-member box
   ui.addToUi(char);
+  //Display the success alert
+  ui.createAlert(`${name.innerText} has been added to your team`, 'success')
   // Clear the fields in the selection box
   name.innerText = '';
   eyecol.innerText = '';
@@ -118,6 +158,10 @@ class UI {
     alertUI.appendChild(alert);
     alertUI.style.visibility = 'visible';
     console.log(alertUI)
+    setTimeout(() => {
+      alertUI.style.visibility = 'hidden';
+      alert.remove();
+    },2500)
   }
 
 }
@@ -127,5 +171,8 @@ document.body.addEventListener('click', (e) => {
  const ui = new UI(e);
  ui.removeFromUi(e);
 })
+
+
+
 
 
