c4a9fc954c997760111c93720aaabceb488883be
Admin <admin@Michaels-MacBook-Pro.local>
2020-06-19 23:06:18 +0200

    inlude populate films and starships functions

--- app1.js
+++ app1.js
@@ -2,23 +2,30 @@ const swapi = new Swapi;
 const ui = new UI;
 characters = [];
 
+
+
+window.onload = (event) => {
+  swapi.getCharacters()
+  .then((res) => {
+    for (let i = 0; i < 8; i ++) {
+      for (let j = 0; j < 10; j ++) {
+        characters.push(res[i].data.results[j])
+      }
+    }
+    console.log(res);
+  })
+};
+
 view.addEventListener('click', (e) => {
-  
     if (input.value === '') {
       alert('cannot be blank')
     } else {
-      swapi.getCharacters()
-      .then((res) => {
-        for (let i = 0; i < 8; i ++) {
-          for (let j = 0; j < 10; j ++) {
-            characters.push(res[i].data.results[j])
-          }
-        }
-      })
       characters.forEach((char) => {
         if (char.name === input.value) {
           ui.populateName(char);
           ui.populatePlanet(char);
+          ui.populateStarships(char);
+          ui.populateFilms(char);
         }
       })
       
--- ui.js
+++ ui.js
@@ -7,13 +7,46 @@ const films = document.querySelector('.td-films')
 
 class UI {
   populateName(char) {
-        name.innerText = char.name
+    name.innerText = char.name
   }
 
   populatePlanet(char) {
-        axios.get(char.homeworld)
-        .then((res) => {
-          home.innerText = res.data.name;
-        })
+    axios.get(char.homeworld)
+      .then((res) => {
+        home.innerText = res.data.name;
+    })
   }
-}
+
+  async populateStarships(char) {
+    const starships = [];
+      char.starships.forEach((ship) => {
+        starships.push(axios.get(ship).then(res => res.data.name));
+      })
+    const shipResults = await Promise.all(starships);
+    if (shipResults.length === 0) {
+      ships.innerText = 'None';
+    } else {
+      const html = shipResults.map((obj) => {
+        return `<span> ${obj}</span>`
+      })
+      ships.innerHTML = html;
+    }
+  }
+
+  async populateFilms(char) {
+    const filmsArr = [];
+    char.films.forEach((film) => {
+      filmsArr.push(axios.get(film).then(res => res.data.title))
+    }) 
+      const filims = await Promise.all(filmsArr);
+          if (filims.length === 0) {
+            films.innerText = 'None';
+          } else {
+            const html = filims.map((obj) => {
+              return `<span> ${obj}</span>`
+            })
+            films.innerHTML = html;
+          }
+        }
+    }
+  
